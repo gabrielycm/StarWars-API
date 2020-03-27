d38066900705aa5ccce60d7ffe80ec72695f2688
Gaby <lubaaenzin@gmail.com>
2020-03-27 20:35:53 -0300

    Updates

---
+++ precache-manifest.65160939d278aab26f6d378a19605f68.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab487e253f263ab9ee21a9a4c9849652",
+    "url": "/react-hooks-e-redux/index.html"
+  },
+  {
+    "revision": "0140b7638ee189e3c96a",
+    "url": "/react-hooks-e-redux/static/css/main.b0db275e.chunk.css"
+  },
+  {
+    "revision": "7f52e4d2dc0bbb0d8e91",
+    "url": "/react-hooks-e-redux/static/js/2.e06f95b2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-hooks-e-redux/static/js/2.e06f95b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0140b7638ee189e3c96a",
+    "url": "/react-hooks-e-redux/static/js/main.5b7bd423.chunk.js"
+  },
+  {
+    "revision": "c55f4489f3b4e9052719",
+    "url": "/react-hooks-e-redux/static/js/runtime-main.8df42135.js"
+  },
+  {
+    "revision": "5d1001864d6c0efe802f0964d8b54325",
+    "url": "/react-hooks-e-redux/static/media/Starlogo.5d100186.png"
+  },
+  {
+    "revision": "ae3170261f37899775bbdda582b45af2",
+    "url": "/react-hooks-e-redux/static/media/dartIcon.ae317026.png"
+  },
+  {
+    "revision": "1ed69097fdd6325db36be06bc69d6e44",
+    "url": "/react-hooks-e-redux/static/media/stargif.1ed69097.gif"
+  }
+]);
